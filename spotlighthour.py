bbca9984b90b4006c8a5a32e27960ddf09a89429
muckelba <34460584+muckelba@users.noreply.github.com>
2020-07-21 01:45:57 +0200

    remove the crawler and just read a json

--- spotlighthour.py
+++ spotlighthour.py
@@ -1,5 +1,4 @@
 import requests
-from bs4 import BeautifulSoup
 import datetime
 from dateutil.parser import parse
 import json
@@ -8,7 +7,7 @@ import configparser
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-url = 'https://leekduck.com/events/'
+url = config['main']['event_json']
 madmin = config['main']['madmin']
 auth_user = config['main']['auth_user']
 auth_pass = config['main']['auth_pass']
@@ -19,34 +18,31 @@ def event_exists(name):
         if name in event["event_name"]:
             return True
 
-
 def get_spotlights(url):
-    content = requests.get(url).text
+    event_json = requests.get(url).json()
     
-    soup = BeautifulSoup(content, "html.parser")
-    for eventitem in soup.find_all(lambda tag: tag.name == 'a' and tag.get('class') == ['event-item-link']):
-        if eventitem["href"].startswith("/events/pokemonspotlighthour"):
-            name = eventitem.find('h2').get_text()
-            dt = parse(eventitem.find('div', {'class': 'event-countdown'})["data-countdown"])
-            print(f"[INFO] {name} found: {dt.strftime('%H:%M:%S %d.%m.%Y')}")
-            
-            if event_exists(name):
-                print(f"[INFO] {name} aready present, skipping...")
-                continue
-            print("[INFO] Creating event: " + name)
-            end = dt + datetime.timedelta(hours=1)
-            data = {
-                'event_name': name,
-                'event_start_date': dt.strftime("%Y-%m-%d"),
-                'event_start_time': dt.strftime("%H:%M"),
-                'event_end_date': dt.strftime("%Y-%m-%d"),
-                'event_end_time': end.strftime("%H:%M"),
-                'event_lure_duration': '30'
-            }
-            response = requests.post(madmin + '/save_event', data=data, auth=(auth_user, auth_pass))
-            if response.status_code == 200:
-                print("[SUCCESS] Successfully created event " + name)
-            else:
-                print("[ERROR] Error while creating event: " + str(response.status_code))
+    for eventitem in event_json:
+        name = eventitem["name"]
+        start = parse(eventitem["start"])
+        end = parse(eventitem["end"])
+        print(f"[INFO] {name} found: {start.strftime('%H:%M:%S %d.%m.%Y')}")
+        
+        if event_exists(name):
+            print(f"[INFO] {name} aready present, skipping...")
+            continue
+        print("[INFO] Creating event: " + name)
+        data = {
+            'event_name': name,
+            'event_start_date': start.strftime("%Y-%m-%d"),
+            'event_start_time': start.strftime("%H:%M"),
+            'event_end_date': end.strftime("%Y-%m-%d"),
+            'event_end_time': end.strftime("%H:%M"),
+            'event_lure_duration': '30'
+        }
+        response = requests.post(madmin + '/save_event', data=data, auth=(auth_user, auth_pass))
+        if response.status_code == 200:
+            print("[SUCCESS] Successfully created event " + name)
+        else:
+            print("[ERROR] Error while creating event: " + str(response.status_code))
 
 get_spotlights(url)
